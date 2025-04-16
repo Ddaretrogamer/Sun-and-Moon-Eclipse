4de9aadcbcf2588dfd18ff00c3b1ef25dee7f7a1
Phantonomy <131238004+Ddaretrogamer@users.noreply.github.com>
2025-04-16 01:16:35 -0500

    adding nameplates to textbox

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -996,6 +996,10 @@ void SetMapVarsToTrainerB(void)
     {
         gSpecialVar_LastTalked = TRAINER_BATTLE_PARAM.objEventLocalIdB;
         gSelectedObjectEvent = GetObjectEventIdByLocalIdAndMap(TRAINER_BATTLE_PARAM.objEventLocalIdB, gSaveBlock1Ptr->location.mapNum, gSaveBlock1Ptr->location.mapGroup);
+        if (gTrainerBattleParameter.params.opponentA != 0) 
+        {
+            gSpeakerName = gTrainers[gTrainerBattleParameter.params.opponentA]->trainerName;
+        }
     }
 }
 
@@ -1442,11 +1446,16 @@ static const u8 *ReturnEmptyStringIfNull(const u8 *string)
 
 static const u8 *GetIntroSpeechOfApproachingTrainer(void)
 {
-    if (gApproachingTrainerId == 0)
-        return ReturnEmptyStringIfNull(TRAINER_BATTLE_PARAM.introTextA);
-    else
-        return ReturnEmptyStringIfNull(TRAINER_BATTLE_PARAM.introTextB);
+    if (gApproachingTrainerId == 0) {
+        gSpeakerName = gTrainers[gTrainerBattleParameter.params.opponentA]->trainerName;
+        return ReturnEmptyStringIfNull(gTrainerBattleParameter.params.introTextA);
+    }
+    else {
+        gSpeakerName = gTrainers[gTrainerBattleParameter.params.opponentB]->trainerName;
+        return ReturnEmptyStringIfNull(gTrainerBattleParameter.params.introTextB);
+    }
 }
+
 
 const u8 *GetTrainerALoseText(void)
 {
