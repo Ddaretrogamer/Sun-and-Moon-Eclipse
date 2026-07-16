e91b96b4ec64be6674a29e0a3042c36d768f70fe
Montblanc <montmoguri@gmail.com>
2026-07-16 00:32:31 -0700

    Implement USUM map popup; use GEN_7 for OW_POPUP_GENERATION

--- src/menu.c
+++ src/menu.c
@@ -591,6 +591,8 @@ u8 AddMapNamePopUpWindow(void)
     {
         if (OW_POPUP_GENERATION == GEN_5)
             sMapNamePopupWindowId = AddWindowParameterized(0, 0, 0, 30, 3, 14, 0x107);
+        else if (OW_POPUP_GENERATION == GEN_7)
+            sMapNamePopupWindowId = AddWindowParameterized(0, 0, DISPLAY_TILE_HEIGHT - 4, 30, 4, 14, 0x107);
         else
             sMapNamePopupWindowId = AddWindowParameterized(0, 1, 1, 10, 3, 14, 0x107);
     }
