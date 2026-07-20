65a10cbe0ce6ff0b49e0d564e277b000b884f05e
Phantonomy <131238004+Ddaretrogamer@users.noreply.github.com>
2026-07-19 20:45:02 -0700

    adding parameter to allow for sequential help windows
    mode 0 is open close
    mode 1 is open to be followed by a
    mode 2 to close.

--- src/help_window.c
+++ src/help_window.c
@@ -1,4 +1,5 @@
 #include "global.h"
+#include "main.h"
 #include "menu.h"
 #include "string_util.h"
 #include "sound.h"
@@ -18,21 +19,28 @@ static const struct WindowTemplate sHelpWindowTemplate = {
     .paletteNum = 15,
     .baseBlock = 8
 };
+static bool8 sHelpWindowIsOpen = FALSE;
+static bool8 sHelpWindowPlaySelectSound;
 
-void ShowHelpInfoWindow(struct ScriptContext *ctx)
+static bool8 WaitForHelpWindowAorBPress(void)
 {
-    u16 helpTutorialId = ScriptReadHalfword(ctx);
-    PlaySE(SE_RG_HELP_OPEN);
-    u32 xOffset;
-    u32 yOffset;
-    yOffset = 1;
-    xOffset = 0;
+    if (JOY_NEW(A_BUTTON) || JOY_NEW(B_BUTTON))
+    {
+        if (sHelpWindowPlaySelectSound)
+            PlaySE(SE_SELECT);
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+static void DrawHelpInfoWindowText(u16 helpTutorialId)
+{
+    u32 xOffset = 0;
+    u32 yOffset = 1;
     u8 headerColor[3] = {0, 8, 3};
     u8 headerFont = FONT_NORMAL;
     u8 descFont = FONT_SMALL;
-
-    helpWindowId = AddWindow(&sHelpWindowTemplate);
-    DrawStdWindowFrame(helpWindowId, FALSE);
 
     // Set non-default values if any
     if (gHelpWindowInfo[helpTutorialId].headerFont)
@@ -40,20 +48,60 @@ void ShowHelpInfoWindow(struct ScriptContext *ctx)
     if (gHelpWindowInfo[helpTutorialId].descFont)
         descFont = gHelpWindowInfo[helpTutorialId].descFont;
 
+    FillWindowPixelBuffer(helpWindowId, PIXEL_FILL(1));
+    PutWindowTilemap(helpWindowId);
+    DrawStdWindowFrame(helpWindowId, FALSE);
+
     // Header Text
     StringCopy(gStringVar4, gHelpWindowInfo[helpTutorialId].header);
     AddTextPrinterParameterized4(helpWindowId, headerFont, xOffset, yOffset, 0, 0, headerColor, 0, gStringVar4);
     yOffset += 16;
+
     // Description (can fiddle with text fonts and what not)
     StringCopy(gStringVar4, gHelpWindowInfo[helpTutorialId].desc);
     AddTextPrinterParameterized(helpWindowId, descFont, gStringVar4, 0, yOffset, TEXT_SKIP_DRAW, NULL);
-
     CopyWindowToVram(helpWindowId, COPYWIN_FULL);
+}
+
+void ShowHelpInfoWindow(struct ScriptContext *ctx)
+{
+    u16 helpTutorialId = ScriptReadHalfword(ctx);
+    u8 mode = ScriptReadByte(ctx);
+    bool8 replace = (mode != 0);
+
+    sHelpWindowPlaySelectSound = (mode == 1);
+
+    if (!sHelpWindowIsOpen)
+    {
+        PlaySE(SE_RG_HELP_OPEN);
+        helpWindowId = AddWindow(&sHelpWindowTemplate);
+        sHelpWindowIsOpen = TRUE;
+    }
+    else if (!replace)
+    {
+        HideHelpInfoWindow();
+        PlaySE(SE_RG_HELP_OPEN);
+        helpWindowId = AddWindow(&sHelpWindowTemplate);
+        sHelpWindowIsOpen = TRUE;
+    }
+
+    DrawHelpInfoWindowText(helpTutorialId);
+}
+
+void WaitForHelpWindowButtonPress(struct ScriptContext *ctx)
+{
+    Script_RequestEffects(SCREFF_V1 | SCREFF_HARDWARE);
+    SetupNativeScript(ctx, WaitForHelpWindowAorBPress);
+    ctx->waitAfterCallNative = TRUE;
 }
 
 void HideHelpInfoWindow(void)
 {
+    if (!sHelpWindowIsOpen)
+        return;
+
     PlaySE(SE_RG_HELP_CLOSE);
     ClearStdWindowAndFrame(helpWindowId, FALSE);
     RemoveWindow(helpWindowId);
+    sHelpWindowIsOpen = FALSE;
 }
